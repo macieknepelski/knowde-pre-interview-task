8b8cf7ce73b915cd726301910ba2fd371b95ef9e
Maciek (Windows) <maciejnepelski1@gmail.com>
2022-09-18 21:00:45 +0200

    Replace hardcoded values with variables

--- cypress/e2e/Catfact.cy.ts
+++ cypress/e2e/Catfact.cy.ts
@@ -8,12 +8,15 @@ describe(`Catfact`, () => {
   it(`TC05: Cat fact API check`, () => {
     cy.request(testData.routes.randomFact).then((response) => {
       expect(response.status).equal(200);
-      expect(response.body).property('fact').and.be.a('string');
-      expect(response.body).property('length').and.be.a('number');
+      Object.entries(testData.factResponseSchema).forEach(([propertyName, propertyType]) => {
+        expect(response.body).property(propertyName).and.be.a(propertyType);
+      })
+      // expect(response.body).property('fact').and.be.a('string');
+      // expect(response.body).property('length').and.be.a('number');
       cy.log(response.body.fact);
       expect(response.body.fact).to.not.be.empty;
       cy.log(response.body.length);
       expect(response.body.length).to.not.be.null;
-  });
-})
+    });
+  })
 })
